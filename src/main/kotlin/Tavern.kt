e7555e6ce3f1fb5acf4f1065e5881baf32a0b445
JavAndy58 <digital.designs.russia@gmail.com>
2023-07-21 17:00:56 +0300

    MutableSet, MutableList

--- src/main/kotlin/Tavern.kt
+++ src/main/kotlin/Tavern.kt
@@ -2,43 +2,43 @@ import java.io.File
 
 private const val TAVERN_MASTER = "Taernyl"
 private const val TAVERN_NAME = "$TAVERN_MASTER's Folly"
-
+private val firstNames = setOf("Alex", "Mordoc", "Sophie", "Tariq")
+private val lastNames = setOf("Ironfoot", "Fernsworth", "Baggins", "Downstrider")
 private val menuDate = File("data/tavern-menu-data.txt")
     .readText()
     .split("\n")
+private val menuItems = List(menuDate.size) {index ->
+    val ( _ , name , _ ) = menuDate[index].split(",")
+    name
+}
 
 fun visitTavern() {
     narrate("$heroName enters $TAVERN_NAME")
-    val patrons = mutableListOf("Eli", "Mordoc", "Sophie")
-
-    val eliMessage = if (patrons.contains("Eli")) {
-        "$TAVERN_MASTER says: Eli's in the back playing cards"
-    } else {
-        "$TAVERN_MASTER says: Eli isn't here"
+    narrate("There are several items for sale:")
+    narrate(menuItems.joinToString())
+    val patrons: MutableSet<String> = mutableSetOf()
+    repeat(10) {
+        patrons += "${firstNames.random()} ${lastNames.random()}"
     }
-    println(eliMessage)
 
-    val othersMessage = if (patrons.containsAll(listOf("Sophie", "Mordoc"))) {
-        "$TAVERN_MASTER says: Sophie and Mordoc are seated by the stew kettle"
-    } else {
-        "$TAVERN_MASTER says: Sophie and Mordoc aren't with each other right now"
+    narrate("$heroName sees several patrons in the tavern:")
+    narrate(patrons.joinToString())
+    repeat(3) {
+        placeOrder(patrons.random(), menuItems.random())
     }
-    println(othersMessage)
 
 //    patrons.forEach{patron ->
 //        println("Good evening, $patron")
 //    }
-
-    patrons.forEachIndexed() { i, patron ->
-        println("Good evening, $patron - you're #${i + 1} in line")
-        placeOrder(patron, "Dragon's Breath")
-    }
-
-    menuDate.forEachIndexed { index, data ->
-        println("$index : $data")
-    }
+//    patrons.forEachIndexed() { i, patron ->
+//        println("Good evening, $patron - you're #${i + 1} in line")
+//        placeOrder(patron, menuItems.random())
+//    }
+//
+//    menuDate.forEachIndexed { index, data ->
+//        println("$index : $data")
+//    }
 }
-
 private fun placeOrder(patronName: String, menuItemName: String) {
     narrate("$patronName speaks with $TAVERN_MASTER to place an order")
     narrate("$TAVERN_MASTER hands $patronName a $menuItemName")
